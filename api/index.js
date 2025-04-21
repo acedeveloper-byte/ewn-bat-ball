64a7e48b58bcf4e0ad9f2fe1620374e8443fdec6
Mayank-Anonymously <craftaar0@gmail.com>
2025-04-21 13:31:29 +0530

    added chanhes

--- api/index.js
+++ api/index.js
@@ -1,24 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const serverless = require('serverless-http');
-const router = require('../src/router/resultRoutes');
-const cors = require('cors');
-const loginrouter = require('../src/router/authRouter.js');
-require('../src/config/dbconnect.js');
+const serverless = require("serverless-http");
+const router = require("../src/router/resultRoutes");
+const cors = require("cors");
+const loginrouter = require("../src/router/authRouter.js");
+require("../src/config/dbconnect.js");
 // Middleware
 app.use(cors());
 
 app.use(express.json());
-app.use('/api', router);
-app.use('/api', loginrouter);
+app.use("/api", router);
+app.use("/api", loginrouter);
 
 // Sample route
-app.get('/api/hello', (req, res) => {
-	res.json({ message: 'Hello from Vercel Serverless!' });
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from Vercel Serverless!" });
 });
 
-// app.listen(5000);
+app.listen(5000);
 // Export handler for Vercel
 module.exports = app;
 
-module.exports.handler = serverless(app);
+// module.exports.handler = serverless(app);
